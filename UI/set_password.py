78ae738fe3cb5cb2372162df9ac11b2ae79fbb5f
袁博实 <542629877@qq.com>
2020-05-03 17:42:30 +0800

    Modified UI

--- UI/set_password.py
+++ UI/set_password.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'set_passwd.ui'
+# Form implementation generated from reading ui file 'set_password.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.2
 #
--- UI/verify_password.py
+++ UI/verify_password.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'enter_passwd.ui'
+# Form implementation generated from reading ui file 'verify_password.ui'
 #
 # Created by: PyQt5 UI code generator 5.14.2
 #
