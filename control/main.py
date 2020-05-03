09a30ba94b97d96568a2e5df57c78b2385c64e10
袁博实 <542629877@qq.com>
2020-05-03 19:39:55 +0800

    minor modifications

--- control/main.py
+++ control/main.py
@@ -3,15 +3,13 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5.QtGui import QIcon
 
-from control.logindialog import LoginDialog
+from control.login import LoginDialog
 
 
 class Main(object):
     def __init__(self):
         app = QApplication(sys.argv)
         app.setWindowIcon(QIcon('ico/app.png'))
-
-        app.setObjectName('123')
 
         self.login_dialog = LoginDialog()
         self.login_dialog.show()
