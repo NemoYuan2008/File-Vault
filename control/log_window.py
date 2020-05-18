a5c9394396bb72bad2e5d0350b611784f08ba838
袁博实 <542629877@qq.com>
2020-05-18 17:10:52 +0800

    Added log exportation

--- control/log_window.py
+++ control/log_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QFileDialog, QMessageBox
 
 from UI.log_window import Ui_log_window
 from file_op import open_log, clear_log
@@ -11,3 +11,14 @@ class LogWindow(QWidget):
         self.ui.setupUi(self)
         self.ui.textBrowser.setText(open_log())
         self.ui.btn_clear.clicked.connect(clear_log)
+        self.ui.btn_export.clicked.connect(self.__export_log)
+
+    def __export_log(self):
+        file_name = QFileDialog.getSaveFileName(self, '选择保存路径', filter='*.log')[0]
+        if file_name:
+            try:
+                with open(file_name, 'w') as f:
+                    f.write(open_log())
+                QMessageBox.information(self, '导出成功', '日志已成功导出到%s' % file_name)
+            except OSError:
+                QMessageBox.warning(self, '导出失败', '日志导出失败, 请换一个路径再试')
